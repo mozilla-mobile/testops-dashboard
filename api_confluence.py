24039f917c28125d28405faad8cb9c7f4ce1028f
Richard Pappalardo <rpappalax@gmail.com>
2025-03-19 16:32:29 +0100

    path fix

--- api_confluence.py
+++ api_confluence.py
@@ -25,7 +25,7 @@ confluence = Confluence(
 # PAGE_ID = "419954941"
 PAGE_ID = "1346961433"
 page_url = f"{BASE_URL}/content/{PAGE_ID}"
-YAML_FILE_PATH = "confluence-reports.yaml"
+YAML_FILE_PATH = "confluence/confluence-reports.yaml"
 IMAGE_PATH = "images"
 
 
