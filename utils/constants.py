8327dfd196567ca49fc6efba32d8d9bb4cac505c
Richard Pappalardo <rpappalax@gmail.com>
2025-03-26 13:24:40 +0100

    module naming refactor (#80)
    * module naming refactor
    
    * rename modules for consistency
    
    * missing comma

--- utils/constants.py
+++ utils/constants.py
@@ -25,15 +25,15 @@ PROJECTS_MOBILE = [
 ]
 
 REPORT_TYPES = [
-    'test-case-coverage',
-    'test-run-counts',
-    'issue-regression',
+    'testrail-test-case-coverage',
+    'testrail-test-run-counts',
+    'testrail-milestones',
     'jira-qa-requests',
     'jira-qa-needed',
+    'jira-softvision-worklogs',
     'bugzilla-qe-verify',
-    'testrail-milestones',
     'confluence-updates',
-    'jira-softvision-worklogs'
+    'github-issue-regression',
 ]
 
 # JQL query options
