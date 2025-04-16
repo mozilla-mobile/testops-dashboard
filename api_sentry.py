623772584a9e85af72f6f1889908ccfd621cd12c
Clare So <cso@mozilla.com>
2025-04-16 17:32:52 -0400

    Recreated database. Could we insert sentry_id now?

--- api_sentry.py
+++ api_sentry.py
@@ -102,7 +102,7 @@ class DatabaseSentry():
     def report_issue_payload(self, issues, release_version):
         payload = []
         for issue in issues:
-            sentry_id = issue['sentry_id']
+            sentry_id = issue['id']
             culprit = issue['culprit']
             title = issue['title']
             permalink = issue['permalink']
