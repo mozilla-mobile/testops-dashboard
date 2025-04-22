e2da02675628f715db904651f7f5f6033b93b860
Clare So <cso@mozilla.com>
2025-04-22 14:57:32 -0400

    Can we paginate?

--- lib/sentry_conn.py
+++ lib/sentry_conn.py
@@ -1,4 +1,5 @@
 import requests
+from requests_toolbelt.utils.links import parse_link_header
 
 
 class APIClient:
@@ -11,13 +12,34 @@ class APIClient:
                 '{0}api/0/'
             ).format(base_url)
 
-    def get(self, uri):
-        headers = {
-            'Authorization': f'Bearer {self.api_token}',
-            'Content-Type': 'application/json'
-        }
-        url = self.__url + uri
-        print(url)
+from requests_toolbelt.utils.links import parse_link_header
+import requests
+
+def get(self, uri):
+    headers = {
+        'Authorization': f'Bearer {self.api_token}',
+        'Content-Type': 'application/json'
+    }
+
+    url = self.__url + uri
+    all_results = []
+
+    while url:
+        print(f"Fetching: {url}")
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an error for bad status codes
-        return response.json()
+        response.raise_for_status()
+
+        data = response.json()
+        if isinstance(data, list):
+            all_results.extend(data)
+        else:
+            return data  # For non-list endpoints
+
+        link_header = response.headers.get("Link")
+        links = parse_link_header(link_header) if link_header else {}
+        next_link = links.get("next")
+
+        url = next_link["url"] if next_link and next_link.get("results", "false").lower() == "true" else None
+
+    return all_results
+
