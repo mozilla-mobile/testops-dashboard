8a6f02f016005bbe596ebd648a2eca894d98c588
Clare So <cso@mozilla.com>
2025-04-23 00:44:25 -0400

    remove ":is"

--- api_sentry.py
+++ api_sentry.py
@@ -36,7 +36,7 @@ class Sentry:
             (
                 'organizations/{0}/issues/'
                 '?project={1}'
-                '&query=is:release.version:{2}'
+                '&query=release.version:{2}'
                 # '&query=is:for_review release.version:{2}'
                 '&sort=freq&statsPeriod=1d'
             ).format(self.organization_slug, self.project_id, release_version)
