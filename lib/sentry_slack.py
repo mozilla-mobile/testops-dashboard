33e24467e7cad277524f28284462b6f48f020d9d
Clare So <1740517+clarmso@users.noreply.github.com>
2025-05-03 01:28:34 -0400

    Print version numbers

--- lib/sentry_slack.py
+++ lib/sentry_slack.py
@@ -6,12 +6,21 @@ def all_available_versions():
     csv_files = Path('.').glob('*.csv')
     for file in csv_files:
         print(f"File: {file.name}, Suffix: {file.stem}")
-        version = file.name.split('sentry_issues_')
+        version = file.name.split('sentry_issues_')[-1].split('.csv')[0]
         versions.append(version)
     return versions
 
 def insert_json_content(json_data, versions):
-    pass
+    for version in versions:
+        this_version = {
+		      "type": "section",
+		        "text": {
+			        "type": "mrkdwn",
+			        "text": f"Release: v{version}"
+		        }
+	        }
+        json_data["blocks"].append(this_version)
+    return json_data
 
 def init_json():
     json_data = {
@@ -30,7 +39,9 @@ def init_json():
 
 def main():
     versions = all_available_versions()
+    print(versions)
     json_data = init_json()
+    insert_json_content(json_data, versions)
     with open('sentry_slack.json', 'w') as json_file:
         json.dump(json_data, json_file, indent=4)
     
