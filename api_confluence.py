9a3918b257212d2528281d48f719bdeedfceb0fe
Richard Pappalardo <rpappalax@gmail.com>
2025-07-14 09:45:30 +0200

    Refactor api_confluence - phase 1 (#131)
    * initial commit add XML update
    
    * api_confluence changes
    
    * add input params
    
    * more api_confluence.py cleanup
    
    * update requirements.txt
    
    * Trigger CI
    
    * exclude workflows from flake8
    
    * change name of temp workflow

--- api_confluence.py
+++ api_confluence.py
@@ -4,12 +4,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
 import os
 import glob
 import json
 import yaml
 from atlassian import Confluence
 import requests
+from bs4 import BeautifulSoup
+from jinja2 import Template
 
 
 # Confluence ENV vars
@@ -26,8 +29,15 @@ confluence = Confluence(
     password=ATLASSIAN_API_TOKEN
 )
 
-PATH_IMAGES = "config/confluence/images"
-PATH_YAML_FILES = "config/confluence"
+PATH_CONFIG = "config/confluence"
+PATH_IMAGES = f"{PATH_CONFIG}/images"
+PATH_YAML_FILES = f"{PATH_CONFIG}/yaml"
+PATH_XML_FILES = f"{PATH_CONFIG}/xml"
+
+
+# ------------------------------------------------------------------
+# URL string builders
+# ------------------------------------------------------------------
 
 
 def url_page(page_id):
@@ -35,7 +45,68 @@ def url_page(page_id):
 
 
 def url_attachments(page_id):
-    return f"{URL_WIKI_REST_API}/content/{page_id}/child/attachment"
+    # return f"{URL_WIKI_REST_API}/content/{page_id}/child/attachment"
+    path = url_page(page_id)
+    return f"{path}/child/attachment"
+
+
+def url_page_content_storage(page_id):
+    path = url_page(page_id)
+    return f"{path}?expand=body.storage"
+
+
+# ------------------------------------------------------------------
+# Page handlers
+# ------------------------------------------------------------------
+
+
+def page_object(page_url):
+    """give page url, returns page object as JSON
+    """
+
+    response = requests.get(page_url, auth=auth, headers=headers)
+    if response.status_code != 200:
+        print("Failed to retrieve the page")
+        print(response.text)
+        exit()
+    return response.json()
+
+
+def page_payload_write(page_id, update_payload):
+    update_url = f"{URL_WIKI_REST_API}/content/{page_id}"
+    headers.update({"Content-Type": "application/json"})
+
+    update_response = requests.put(update_url, auth=auth, headers=headers,
+                                   data=json.dumps(update_payload))
+
+    if update_response.status_code == 200:
+        print("Page updated successfully!")
+    else:
+        print("Failed to update the page")
+        print(update_response.text)
+
+
+def page_payload(page_id, page_title, page_data, current_version, new_content):
+    # Update the page with new content
+    update_payload = {
+        "id": page_id,
+        "type": "page",
+        "title": page_title,
+        "space": {"key": page_data["space"]["key"]},
+        "body": {
+            "storage": {
+                "value": new_content,
+                "representation": "storage"
+            }
+        },
+        "version": {"number": current_version + 1}  # Increment version
+    }
+    return update_payload
+
+
+# ------------------------------------------------------------------
+# Attachment processing
+# ------------------------------------------------------------------
 
 
 def image_attachments_delete(page_id):
@@ -98,17 +169,9 @@ def image_attachments_upload(page_id):
 
     print("Upload process completed!")
 
-
-def page_object(page_url):
-    """
-    give page url, returns page object as JSON
-    """
-    response = requests.get(page_url, auth=auth, headers=headers)
-    if response.status_code != 200:
-        print("Failed to retrieve the page")
-        print(response.text)
-        exit()
-    return response.json()
+# ------------------------------------------------------------------
+# Page rendering: Looker Reports (YAML)
+# ------------------------------------------------------------------
 
 
 def table_row_write(report_title, report_description,
@@ -123,7 +186,6 @@ def table_row_write(report_title, report_description,
         """ # noqa
 
 
-# def page_html(image_name, page_id):
 def page_html(page_id, sections):
 
     html_content = ""
@@ -147,41 +209,9 @@ def page_html(page_id, sections):
     return html_content
 
 
-def page_payload(page_id, page_title, page_data, current_version, new_content):
-    # Update the page with new content
-    update_payload = {
-        "id": page_id,
-        "type": "page",
-        "title": page_title,
-        "space": {"key": page_data["space"]["key"]},
-        "body": {
-            "storage": {
-                "value": new_content,
-                "representation": "storage"
-            }
-        },
-        "version": {"number": current_version + 1}  # Increment version
-    }
-    return update_payload
-
-
-def page_payload_write(page_id, update_payload):
-    update_url = f"{URL_WIKI_REST_API}/content/{page_id}"
-    headers.update({"Content-Type": "application/json"})
-
-    update_response = requests.put(update_url, auth=auth, headers=headers,
-                                   data=json.dumps(update_payload))
-
-    if update_response.status_code == 200:
-        print("Page updated successfully!")
-    else:
-        print("Failed to update the page")
-        print(update_response.text)
-
-
-def pages():
-    """
-    iterates over confluence YAML files and generates pages
+def pages_looker_graphs():
+    """iterates over confluence YAML config files and generates
+    looker graph pages in bulk (all have same format)
     """
     for filepath in glob.glob(f"{PATH_YAML_FILES}/*.yaml"):  # Only YAML files
         with open(filepath, 'r', encoding='utf-8') as file:
@@ -209,8 +239,169 @@ def pages():
             page_payload_write(page_id, payload)
 
 
+# ------------------------------------------------------------------
+# Page rendering: Custom pages (XML)
+# ------------------------------------------------------------------
+
+
+def render_xml_template(template_path, params):
+    """ 1. open Mustache-style (jinja compatible) XML template
+        2. parameterize template with params """
+
+    with open(template_path, 'r') as file:
+        template_content = file.read()
+
+    template = Template(template_content)
+    rendered_xml = template.render(**params)
+
+    return rendered_xml
+
+
+# UTILITY FUNCTIONS
+
+def page_content_retrieve_xml(page_id):
+    """Utility function only
+
+    Given page_id, write XML content to a local XML template file
+    """
+    page_url = url_page_content_storage(page_id)
+    resp = page_object(page_url)
+
+    soup = BeautifulSoup(resp, "html.parser")
+    pretty_xml = soup.prettify()
+    with open("confluence_page_content.xml", "w", encoding="utf-8") as f:
+        f.write(pretty_xml)
+
+
+def page_content_insert_xml(page_id, params):
+    """Utility function only
+
+    NOTE: Confluence API returns page content in XML format.
+
+    Use this function as a stand-alone utility / example for pulling page
+    content for rewriting pages or creating new XML template configs.
+    """
+
+    TEMPLATE_PATH = f"{PATH_XML_FILES}/build-validation.xml"
+
+    # XML template
+    # with open(TEMPLATE_PATH, "r", encoding="utf-8") as file:
+    #     xml_content = file.read()
+    output_xml = render_xml_template(TEMPLATE_PATH, params)
+
+    # Confluence page (for updating)
+    page_url = url_page(page_id)
+
+    # Write output_xml to page
+    print(f"UPDATING PAGE - page_id: {page_id}")
+    page_data = page_object(page_url)
+    current_version = page_data["version"]["number"]
+
+    payload = page_payload(page_id, page_title, page_data, current_version, output_xml) # noqa
+    page_payload_write(page_id, payload)
+
+
+# REPORT: Build Validation
+
+def page_report_build_validation(
+    page_id,
+    projects_id,
+    testrail_milestone_id,
+    testrail_milestone_title,
+    testrail_report_url,
+    build_version,
+    signoff_date,
+    test_status,
+    test_summary,
+    ship_recommend,
+    ship_recommend_verbose,
+    contacts,
+):
+    """
+    Applies params to jinja/mustache-style XML template and inserts into page
+    """
+
+    # TODO: hard-coded params for now...
+    params = {
+        'release_date': signoff_date,
+        'build_version': build_version,
+        'build_version_commit_hash': '12345',
+        'testrail_report_url': 'https://mozilla.testrail.io/index.php?/runs/view/108599&group_by=cases:section_id&group_order=asc', # noqa
+        'testrail_report_summary': test_summary,
+        'testrail_run_1_result': 'PASS3D',
+        'testrail_run_1_build_type': 'ARM64',
+        'testrail_run_1_device': 'iPhone 16 Pro (iOS 18.3.2)',
+        'testrail_run_2_result': 'PASS3D',
+        'testrail_run_2_build_type': 'ARM64',
+        'testrail_run_2_device': 'iPhone 16 Pro (iOS 18.3.2)',
+        'testrail_issues_verified': 'N/A',
+        'testrail_issues_new': 'N/A',
+        'testrail_issues_known': '<a href="https://mozilla-hub.atlassian.net/browse/FXIOS-12650"</a>', # noqa
+        'testrail_contacts': contacts,
+    }
+
+    params = {
+        **locals()
+    }
+
+    TEMPLATE_PATH = f"{PATH_XML_FILES}/build-validation.xml"
+
+    page_url = url_page(page_id)
+    # page_url_storage = url_page_content_storage(page_id)
+    with open(TEMPLATE_PATH, "r", encoding="utf-8") as file:
+        xml_content = file.read()
+    output_xml = render_xml_template(TEMPLATE_PATH, params)
+
+    print(f"UPDATE PAGE - page_id: {page_id}")
+    page_data = page_object(page_url)
+    current_version = page_data["version"]["number"]
+
+    # new_content = page_html(page_id, page_sections)
+    payload = page_payload(page_id, page_title, page_data, current_version, output_xml) # noqa
+    page_payload_write(page_id, payload)
+
+
 def main():
-    pages()
+    # TODO: phase 2 PR - instead of invoking this directly from main,
+    # invoke it from __main__.py --report-type looker-graphs
+    pages_looker_graphs()
+
+    # TODO: design approach for custom (XML-config) reports
+    """
+    page_id = "1663598593"
+    page_title = "DEMO v2"
+    projects_id = 14 # Firefox for iOS
+    testrail_milestone_id = "1066" # Manual functional testing sign-off - Firefox v120 (36024) RC1 # noqa
+    testrail_report_url = "http://mozilla.org"
+    release = "Manual functional testing sign-off - Firefox v120 (36024) RC1"
+    build_version = "v149"
+    testing_status = "green"
+    testing_summary = '''
+    <li>We started Full Functional test suite.</li>
+    <li>We ran X sets of automated Full Functional tests on iPhone and iPad with XYZ experiment ON.</li> # noqa
+    '''
+    release_tag_url = "https://archive.mozilla.org/pub/fenix/releases/"
+    qa_contacts = '''
+     <a href='mailto:csuciu@mozilla.com'>Catalin Suc1u</a>,
+     <a href='mailto:amoldovan@mozilla.com'>Alina M0ld0van</a>,
+     <a href='mailto:abodea@mozilla.com'>Andr3i Bodea</a>
+     '''
+
+    page_report_build_validation(
+        page_id=page_id,
+        projects_id=projects_id,
+        testrail_milestone_id,
+        testrail_milestone_title=release,
+        testrail_report_url=testrail_report_url,
+        build_version=build_version,
+        signoff_date=release_date,
+        test_status=testing_status,
+        test_summary=testing_summary,
+        ship_recommend=qa_recommendation,
+        ship_recommend_verbose=qa_recommendation_verbose,
+        contacts=qa_contacts,
+    )
+    """
 
 
 if __name__ == "__main__":
