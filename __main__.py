f765129be36f5574d16945408add003a38ea7170
Richard Pappalardo <rpappalax@gmail.com>
2025-08-06 12:38:23 +0200

    fix jira func

--- __main__.py
+++ __main__.py
@@ -158,12 +158,12 @@ def handle_github_issue_regression(args):
 def handle_jira_qa_requests(args):
     client = JiraClient()
     client.jira_qa_requests()
+    client.jira_qa_requests_new_issue_types()
 
 
 def handle_jira_qa_needed(args):
     client = JiraClient()
     client.jira_qa_needed()
-    client.jira_qa_requests_new_issue_types()
 
 
 def handle_jira_softvision_worklogs(args):
