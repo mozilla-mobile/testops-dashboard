b30002e528dcd3dbbe158e4bb904cb1bc0f12eb5
Richard Pappalardo <rpappalax@gmail.com>
2025-08-12 22:15:20 +0200

    safe cutover

--- api/testrail/__init__.py
+++ api/testrail/__init__.py
@@ -1,21 +1,2 @@
-"""
-Package init: temporary shim to keep legacy handler paths working during refactor.
-
-We attach a coverage method to TestRailClient that forwards to the *functional*
-orchestrator function. The functional module itself must *not* call back into
-TestRailClient to avoid recursion.
-
-TODO(PR4): remove this file once handlers call report modules directly.
-"""
-from .service_client import TestRailClient  # local import; class exists here
-
-
-def _testrail_coverage_update_shim(self, *args, **kwargs):
-    # Import inside to avoid import cycles.
-    from .report_testrail_coverage import testrail_coverage_update as _run
-    return _run(*args, **kwargs)
-
-
-# Only attach if it's missing (i.e., the method was commented out during migration)
-if not hasattr(TestRailClient, "testrail_coverage_update"):
-    setattr(TestRailClient, "testrail_coverage_update", _testrail_coverage_update_shim)
+# __init__.py
+# (PR4) Empty init; previous monkey-patch removed.
--- api/testrail/api_testrail.py
+++ api/testrail/api_testrail.py
@@ -1,16 +1,33 @@
-"""Shim: temporary re-exports during TestRail refactor."""
+"""Re-export hub for functional report entry points (compat shim).
 
+TODO(PR5): remove after handlers import report modules directly.
+"""
+from .report_testrail_runs import testrail_runs_update  # noqa: F401
 from .report_testrail_runs import fetch_testrail_runs  # noqa: F401
 from .report_testrail_runs import prepare_testrail_runs  # noqa: F401
 from .report_testrail_runs import insert_testrail_runs  # noqa: F401
-from .report_testrail_runs import testrail_runs_update  # noqa: F401
 
+from .report_testrail_run_counts import testrail_run_counts_update  # noqa: F401
 from .report_testrail_run_counts import fetch_testrail_run_counts  # noqa: F401
 from .report_testrail_run_counts import prepare_testrail_run_counts  # noqa: F401
 from .report_testrail_run_counts import insert_testrail_run_counts  # noqa: F401
-from .report_testrail_run_counts import testrail_run_counts_update  # noqa: F401
 
+from .report_testrail_coverage import testrail_coverage_update  # noqa: F401
 from .report_testrail_coverage import fetch_testrail_coverage  # noqa: F401
 from .report_testrail_coverage import prepare_testrail_coverage  # noqa: F401
 from .report_testrail_coverage import insert_testrail_coverage  # noqa: F401
-from .report_testrail_coverage import testrail_coverage_update  # noqa: F401
+
+from .report_testrail_users import testrail_users_update  # noqa: F401
+from .report_testrail_users import fetch_testrail_users  # noqa: F401
+from .report_testrail_users import prepare_testrail_users  # noqa: F401
+from .report_testrail_users import insert_testrail_users  # noqa: F401
+
+from .report_testrail_milestones import testrail_milestones_update  # noqa: F401
+from .report_testrail_milestones import fetch_testrail_milestones  # noqa: F401
+from .report_testrail_milestones import prepare_testrail_milestones  # noqa: F401
+from .report_testrail_milestones import insert_testrail_milestones  # noqa: F401
+
+from .report_testrail_testplans import testrail_testplans_update  # noqa: F401
+from .report_testrail_testplans import fetch_testrail_testplans  # noqa: F401
+from .report_testrail_testplans import prepare_testrail_testplans  # noqa: F401
+from .report_testrail_testplans import insert_testrail_testplans  # noqa: F401
