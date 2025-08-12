363c8e631df34b6eab38ac49a82fb866efd17f3e
Richard Pappalardo <rpappalax@gmail.com>
2025-08-12 23:14:31 +0200

    ready to update handler

--- handlers/testrail.py
+++ handlers/testrail.py
@@ -1,31 +1,18 @@
-from api.testrail.service_client import TestRailClient
-
-
-def handle_testrail_test_plans_and_runs(args):
-    client = TestRailClient()
-    client.testrail_plans_and_runs(args.project, args.num_days or '30')
-
-
-def handle_testrail_test_results(args):
-    client = TestRailClient()
-    client.testrail_test_results()
-
-
-def handle_testrail_milestones(args):
-    client = TestRailClient()
-    client.testrail_milestones(args.arg_list)
-
-
-def handle_testrail_users(args):
-    client = TestRailClient()
-    client.testrail_users()
-
+from api.testrail import report_testrail_test_case_coverage as coverage
+from api.testrail import report_testrail_milestones as milestones
+from api.testrail import report_testrail_users as users
+from api.testrail import report_testrail_testplans as plans
+from api.testrail import report_testrail_runs as runs
 
 def handle_testrail_test_case_coverage(args):
-    client = TestRailClient()
-    client.data_pump_report_test_case_coverage(args.arg_list)
+    coverage.testrail_coverage_update(args.arg_list)
 
+def handle_testrail_milestones(args):
+    milestones.testrail_milestones_update(args.arg_list)
 
-def handle_testrail_test_run_counts_update(args):
-    client = TestRailClient()
-    client.testrail_run_counts_update(args.project, args.num_days or '')
+def handle_testrail_users(args):
+    users.testrail_users_update()
+
+def handle_testrail_test_plans_and_runs(args):
+    plans.testrail_testplans_update(args.project, args.num_days or '30')
+    runs.testrail_runs_update(args.project, args.num_days or '30')
