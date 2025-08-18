ba51e665ab46bce2426605a4281b64569384bb7d
Richard Pappalardo <rpappalax@gmail.com>
2025-08-18 16:13:09 +0200

    fix workflow

--- handlers/testrail.py
+++ handlers/testrail.py
@@ -3,11 +3,13 @@ import api.testrail.report_users as users
 import api.testrail.report_milestones as milestones
 import api.testrail.report_test_results as test_results
 import api.testrail.report_test_case_coverage as test_case_coverage
+import api.testrail.report_test_plans_and_runs as test_plans_and_runs 
 
 
 def handle_testrail_test_plans_and_runs(args):
     client = TestRailClient()
     client.testrail_plans_and_runs(args.project, args.num_days or '30')
+    #test_plans_and_runs.testrail_plans_and_runs(args.project, args.num_days or '30')
 
 
 def handle_testrail_test_results(args):
