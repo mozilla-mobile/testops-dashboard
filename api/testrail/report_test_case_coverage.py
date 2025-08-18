7e27c2c41f9d90cf9dedfdc8a0bbf2bd2ea65a47
Richard Pappalardo <rpappalax@gmail.com>
2025-08-18 14:31:49 +0200

    separate report_test_case_coverage, p8

--- api/testrail/report_test_case_coverage.py
+++ api/testrail/report_test_case_coverage.py
@@ -55,9 +55,7 @@ def test_suites_delete():
     print("DIAGNOSTIC: Running: report_test_case_coverage ")
     print(inspect.currentframe().f_code.co_name)
 
-    #self.session.query(TestSuites).delete()
     db.session.query(TestSuites).delete()
-    #self.session.commit()
     db.session.commit()
 
 
@@ -65,7 +63,6 @@ def testrail_test_case_coverage(project='all', suite='all'):
     # call database for 'all' values
     # convert inputs to a list so we can easily
     # loop thru them
-    #project_ids_list = self.testrail_project_ids(project)
 
     project_ids_list = testrail_project_ids(project)
     print(project_ids_list)
@@ -75,17 +72,14 @@ def testrail_test_case_coverage(project='all', suite='all'):
 
     # Test suite data is dynamic. Wipe out old test suite data
     # in database before updating.
-    db = _db()
     tr = _tr()
 
-    #self.db.test_suites_delete()
     test_suites_delete()
 
     for project_ids in project_ids_list:
         projects_id = project_ids[0]
 
         testrail_project_id = project_ids[1]
-        #suites = self.test_suites(testrail_project_id)
         suites = tr.test_suites(testrail_project_id)
 
         for suite in suites:
@@ -94,12 +88,8 @@ def testrail_test_case_coverage(project='all', suite='all'):
             print("suite_id: {0}".format(suite['id']))
             print("suite_name: {0}".format(suite['name']))
             """
-            #self.db.test_suites_update(testrail_project_id,
-            #                           suite['id'], suite['name'])
             test_suites_update(testrail_project_id,
                                   suite['id'], suite['name'])
-            #self.testrail_coverage_update(projects_id,
-            #                              testrail_project_id, suite['id'])
             testrail_coverage_update(projects_id,
                                      testrail_project_id, suite['id'])
 
@@ -116,24 +106,18 @@ def test_suites_update(testrail_project_id,
     suites = TestSuites(testrail_project_id=testrail_project_id,
                         testrail_test_suites_id=testrail_test_suites_id,
                         test_suite_name=test_suite_name)
-    #self.session.add(suites)
     db.session.add(suites)
-    #self.session.commit()
     db.session.commit()
 
 
 def testrail_coverage_update(projects_id, testrail_project_id, test_suite_id):
 
-    db = _db()
     tr = _tr()
 
     # Pull JSON blob from Testrail
-    #cases = self.test_cases(testrail_project_id, test_suite_id)
     cases = tr.test_cases(testrail_project_id, test_suite_id)
 
     # Format and store data in a data payload array
-    #payload = self.db.report_test_coverage_payload(cases)
-    #payload = db.report_test_coverage_payload(cases)
     payload = report_test_coverage_payload(cases)
 
     print("-------------------------")
@@ -142,7 +126,6 @@ def testrail_coverage_update(projects_id, testrail_project_id, test_suite_id):
     print(payload)
 
     # Insert data in 'totals' array into DB
-    #self.db.report_test_coverage_insert(projects_id, payload)
     report_test_coverage_insert(projects_id, payload)
 
 
@@ -220,7 +203,5 @@ def report_test_coverage_insert(projects_id, payload):
             test_sub_suites_id=row['sub'],
             test_count=row['tally']
         )
-        #self.session.add(report)
         db.session.add(report)
-        #self.session.commit()
         db.session.commit()
