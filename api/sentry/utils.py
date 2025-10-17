02a23eda2057878b68214afe5ad1cdae89e4bd4f
Clare So <1740517+clarmso@users.noreply.github.com>
2025-10-17 13:47:44 -0400

    MTE-4930 Update icon (#224)

--- api/sentry/utils.py
+++ api/sentry/utils.py
@@ -120,7 +120,7 @@ def insert_rates(json_data, csv_file, project):
                     "elements": [
                         {
                             "type": "mrkdwn",
-                            "text": "⚠️ Low crash-free rate(s) (<{0}%) detected ⚠️"
+                            "text": "❗ Low crash-free rate(s) (<{0}%) detected"
                             .format(low_crash_free_rate_threshold)
                         }
                     ]
