f5d4a608895b6fa998a35c12c4bed5bcd11d4e65
Clare So <1740517+clarmso@users.noreply.github.com>
2026-02-18 14:59:34 -0500

    flake8

--- api/github/api_github.py
+++ api/github/api_github.py
@@ -118,7 +118,8 @@ class Github:
         # Use search API to get newly created issues, not updated issues
         # Exclude data-sync-user directly in the query
         return self.client.http_get(
-            'search/issues?q=repo:{0}/{1}+is:issue+state:open+label:"Bug 🐞"+no:assignee+created:>={2}+-author:data-sync-user'
+            'search/issues?q=repo:{0}/{1}+is:issue+state:open+'
+            'label:"Bug 🐞"+no:assignee+created:>={2}+-author:data-sync-user'
             .format(OWNER, project, timestamp) # noqa
         )
 
